6d6e2dc2a1630ee0196c7a292a1a59b7df5c5785
Poseydon42 <vvmposeydon@gmail.com>
2022-08-21 13:24:30 +0100

    Defined Ray struct

---
+++ Source/Ray.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include <glm/glm.hpp>
+
+namespace Geode
+{
+    struct Ray
+    {
+        glm::vec3 Origin;
+        glm::vec3 Direction;
+    };
+
+    struct RayHit
+    {
+        glm::vec3 Position;
+        glm::vec3 Normal;
+        size_t MaterialIndex;
+    };
+}

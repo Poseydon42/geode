33fb7874eed8ecc46e720fbada421775dbaa4913
Poseydon42 <vvmposeydon@gmail.com>
2022-09-01 18:22:04 +0100

    Added more spheres

--- Source/main.cpp
+++ Source/main.cpp
@@ -5,7 +5,6 @@
 
 #include "Camera.h"
 #include "Material.h"
-#include "Ray.h"
 #include "Renderer.h"
 #include "Surface.h"
 #include "TGAWriter.h"
@@ -21,10 +20,14 @@ namespace Geode
 
         Material Material1 = {{0.5f, 0.7f, 1.0f}};
         Material Material2 = {{0.5f, 0.0f, 1.0f}};
+        Material Material3 = {{0.0f, 0.0f, 1.0f}};
+        Material Material4 = {{0.3f, 0.1f, 0.0f}};
         Sphere Sphere1 = {{0.0f, 1.0f, -5.0f}, 1.0f, 0};
         Sphere Sphere2 = {{1.0f, -0.5f, -5.0f}, 0.4f, 1};
+        Sphere Sphere3 = {{2.0f, 0.0f, -5.0f}, 0.7f, 2};
+        Sphere Sphere4 = {{0.0f, -1.5f, -5.0f}, 0.4f, 3};
 
-        World World = {{Material1, Material2}, {Sphere1, Sphere2}};
+        World World = {{Material1, Material2, Material3, Material4}, {Sphere1, Sphere2, Sphere3, Sphere4}};
 
         auto* Image = malloc(Width * Height * BytesPerPixel);
 
